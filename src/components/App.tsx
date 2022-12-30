1222dab006adecdc8924205ebef95d2736b50bd6
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 07:12:21 +0530

    Refactor to Functional component, setup axios axios used for sending POST request to the backend api server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,51 @@
-// import logo from "./logo.svg";
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
-import React from "react";
-import { CodeEditor } from "./CodeEditor";
-import { CodeProblem } from "./CodeProblem";
+import CodeEditorComponent from "./CodeEditorComponent";
+import CodeProblem from "./CodeProblem";
 
-export class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header>CodeChamp</header>
-                <div className="container">
-                    <div className="left-pane">
-                        <CodeProblem />
-                    </div>
-                    <div className="right-pane">
-                        <CodeEditor />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+type RequestBody = {
+  id: string;
+  languageType: string;
+  codeBody: string;
+};
+export default function App() {
+  // const [requestBody, setRequestBody] = useState<RequestBody>();
+  async function handleSubmit(lang: string, code: string) {
+    console.log(lang);
+    let requestBody: RequestBody = {
+      id: crypto.randomUUID(),
+      languageType: lang,
+      codeBody: code,
+    };
+    console.log(requestBody);
+
+    let headersList = {
+      "Content-Type": "application/json",
+    };
+
+    let response = axios
+      .post("http://localhost:8080/api/compile", JSON.stringify(requestBody), {
+        headers: headersList,
+      })
+      .then((res) => {
+        console.log(res.data);
+        return res.data;
+      });
+
+    console.log(response);
+  }
+  return (
+    <div className="App">
+      <header>CodeChamp</header>
+      <div className="container">
+        <div className="left-pane">
+          <CodeProblem />
+        </div>
+        <div className="right-pane">
+          <CodeEditorComponent handleSubmit={handleSubmit} />
+        </div>
+      </div>
+    </div>
+  );
 }
-
-// export default App;
