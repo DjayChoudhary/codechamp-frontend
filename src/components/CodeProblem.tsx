def5eaaff893acc5225592f1b3a4a85d3480ff99
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 07:12:44 +0530

    Refactor CodeProblem.tsx as functional component

--- src/components/CodeProblem.tsx
+++ src/components/CodeProblem.tsx
@@ -1,15 +1,11 @@
-import React from "react";
-
-export class CodeProblem extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="problem-title">Addition of Two Numbers</div>
-                <div className="description">
-                    <p>Take 2 integers as input and Calculate sum</p>
-                </div>
-                <div className="Examples"></div>
-            </div>
-        );
-    }
+export default function CodeProblem() {
+  return (
+    <div>
+      <div className="problem-title">Addition of Two Numbers</div>
+      <div className="description">
+        <p>Take 2 integers as input and Calculate sum</p>
+      </div>
+      <div className="Examples"></div>
+    </div>
+  );
 }
