303f2e7e7aad409a167e709432ca685faf975fe0
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 07:10:17 +0530

    refactor to functional components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { App } from "./components/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
