54c1123bab07639006c872c074acd62f97e1ffd0
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-30 14:25:55 +0530

    move axios request to src/api

---
+++ src/api/axios.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosHeaders } from "axios";
+import { RequestBody } from "../types/RequestBody";
+
+export default function axiosPostRequest(requestBody: RequestBody) {
+  let headersList = {
+    "Content-Type": "application/json",
+  };
+
+  let response = axios
+    .post("http://localhost:8080/api/compile", JSON.stringify(requestBody), {
+      headers: headersList,
+    })
+    .then((res) => {
+      console.log(res.data);
+      return res.data;
+    });
+
+  return response;
+}
