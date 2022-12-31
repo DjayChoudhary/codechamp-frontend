986f7570168c9898ffd1bae3fd024301e0a27668
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2022-12-31 23:47:15 +0530

    replace div with React Fragment

--- src/components/CodeProblem.tsx
+++ src/components/CodeProblem.tsx
@@ -1,11 +1,11 @@
 export default function CodeProblem() {
   return (
-    <div>
+    <>
       <div className="problem-title">Addition of Two Numbers</div>
       <div className="description">
         <p>Take 2 integers as input and Calculate sum</p>
       </div>
       <div className="Examples"></div>
-    </div>
+    </>
   );
 }
