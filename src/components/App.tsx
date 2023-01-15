29336226480fb41a2539bccade55576f62d2ab88
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-15 23:30:46 +0530

    remove App.css files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,34 @@
-import "./css/App.css";
-import CodeEditorComponent from "./CodeEditorComponent";
+// import CodeEditorComponent from "./CodeEditorComponent";
 import CodeProblem from "./CodeProblem";
 import axiosPostRequest from "../api/axios";
 import { RequestBody } from "../types/RequestBody";
+import Editor from "./Editor";
+import Header from "./Header";
 
 export default function App() {
+  let response;
   function handleSubmit(lang: string, code: string) {
     let requestBody: RequestBody = {
       id: crypto.randomUUID(),
-      languageType: lang,
+      languageType: lang.toUpperCase(),
       codeBody: code,
     };
     console.log(requestBody);
 
-    let response = axiosPostRequest(requestBody);
+    response = axiosPostRequest(requestBody);
 
     console.log(response);
   }
   return (
-    <main className="container">
-      <header>
-        <div className="header">CodeChamp</div>
-      </header>
+    <main className="container bg-gray-400">
+      <Header />
       <section className="app-container">
         <article className="problem-container">
           <CodeProblem />
         </article>
         <section className="editor-container">
-          <CodeEditorComponent handleSubmit={handleSubmit} />
+          {/* <Editor handleSubmit={handleSubmit} response={response} /> */}
+          {/* <CodeEditorComponent handleSubmit={handleSubmit} /> */}
         </section>
       </section>
     </main>
