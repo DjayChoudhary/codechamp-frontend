22a3d8def383ad00aa6843e239d7138e62bec834
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-15 23:52:20 +0530

    Update Problem Description and title

--- src/components/CodeProblem.tsx
+++ src/components/CodeProblem.tsx
@@ -1,11 +1,11 @@
 export default function CodeProblem() {
   return (
-    <>
-      <div className="problem-title">Addition of Two Numbers</div>
+    <article className="mx-5 my-5">
+      <div className="text-xl font-bold underline">Addition of Two Numbers</div>
       <div className="description">
         <p>Take 2 integers as input and Calculate sum</p>
       </div>
       <div className="Examples"></div>
-    </>
+    </article>
   );
 }
