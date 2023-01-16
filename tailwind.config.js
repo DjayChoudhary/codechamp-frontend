9e55ab4f12b89ba9f351bd9aa2cae0d77dae1a21
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-16 22:11:11 +0530

    add google fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
