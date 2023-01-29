6422385516448beafbbae67092738d0dfc078ab7
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-01-30 00:00:10 +0530

    add AuthState TS interface

--- src/contexts/AuthContext.ts
+++ src/contexts/AuthContext.ts
@@ -1,3 +1,10 @@
 import { createContext } from "react";
 
 export const AuthContext = createContext(null);
+
+interface AuthState {
+  isLoggedIn: boolean;
+  isAuthenticated: boolean;
+  userName: string;
+  userId: string;
+}
